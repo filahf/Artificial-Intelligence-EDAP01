dcb5478b78e878ce1526fdd9c3c7143291908000
Filip <filahf94@gmail.com>
2020-02-03 10:53:27 +0100

    Board def done

--- Othello/othello.py
+++ Othello/othello.py
@@ -1,4 +1,4 @@
-#http://dhconnelly.com/paip-python/docs/paip/othello.html#section-8
+# http://dhconnelly.com/paip-python/docs/paip/othello.html#section-8
 
 EMPTY, BLACK, WHITE, OUTER = '.', '@', 'o', '?'
 PIECES = (EMPTY, BLACK, WHITE, OUTER)
@@ -8,8 +8,9 @@ UP, DOWN, LEFT, RIGHT = -10, 10, -1, 1
 UP_RIGHT, DOWN_RIGHT, DOWN_LEFT, UP_LEFT = -9, 11, 9, -11
 DIRECTIONS = (UP, UP_RIGHT, RIGHT, DOWN_RIGHT, DOWN, DOWN_LEFT, LEFT, UP_LEFT)
 
+
 def squares():
-    return [i for i in xrange(11,89) if 1 <= (i % 10) <= 8]
+    return [i for i in range(11, 89) if 1 <= (i % 10) <= 8]
 
 
 def initial_board():
@@ -17,17 +18,19 @@ def initial_board():
     for i in squares():
         board[i] = EMPTY
 
-
-    board[44],board[45] = WHITE, BLACK
-    board[54],board[55] = BLACK, WHITE
+    board[44], board[45] = WHITE, BLACK
+    board[54], board[55] = BLACK, WHITE
     return board
+
 
 def print_board(board):
 
     rep = ''
-    rep += ' %s\n' % ' '.join(map(str,range(1,9)))
-    for row in xrange(1,9):
-        begin,end = 10*row + 1,10*row +9
+    rep += ' %s\n' % ' '.join(map(str, range(1, 9)))
+    for row in range(1, 9):
+        begin, end = 10*row + 1, 10*row + 9
         rep += '%d %s\n' % (row, ' '.join(board[begin:end]))
-    
+
     return rep
+
+print(print_board(initial_board()))
